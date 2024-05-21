a0d1918d01e497d011d112a0d41db2a14893f59a
CLX <twbrandon7@gmail.com>
2024-05-21 23:31:39 +0800

    feat: add alias for source directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
+import path from "node:path";
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import monkey, { cdn } from 'vite-plugin-monkey';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     vue(),
     monkey({
