339db006de0a77e24fdabf54cc34ae75adcfaf1e
Li-Xian Chen <twbrandon7@gmail.com>
2024-05-24 21:35:16 +0800

    refactor(parser): extract common method to base block

---
+++ src/lib/base-block.ts
@@ -0,0 +1,26 @@
+import { Definable } from "./interface/definable";
+import { Example, Examplable } from "./interface/examplable";
+import { filterBoldTextFromElement } from "./parser-util";
+
+export default abstract class BaseBlock implements Definable, Examplable {
+    constructor (protected readonly element: HTMLElement) {}
+    
+    public getEnglishDefinition(): string | null {
+        return this.element.querySelector('.def')?.textContent ?? null;
+    }
+
+    public getZhTwDefinition(): string | null {
+        return this.element.querySelector('.def-body .trans')?.textContent ?? null;
+    }
+
+    public getExamples(): Example[] {
+        const exampleBlocks = this.element.querySelectorAll('.def-body .examp.dexamp');
+        const examples: Example[] = [];
+        exampleBlocks.forEach((exampleBlock) => {
+            const englishExample = filterBoldTextFromElement(exampleBlock.querySelector('.eg') as HTMLElement);
+            const zhTwExample = exampleBlock.querySelector('.trans')?.textContent ?? '';
+            examples.push({ englishExample, zhTwExample });
+        });
+        return examples;
+    }
+}
--- src/lib/definition-block.ts
+++ src/lib/definition-block.ts
@@ -1,15 +1,7 @@
 import { CefrLevel } from "./cefr-level";
-import { Definable } from "./interface/definable";
-import { Example, Examplable } from "./interface/examplable";
-import { filterBoldTextFromElement } from "./parser-util";
+import BaseBlock from "./base-block";
 
-export default class DefinitionBlock implements Examplable, Definable {
-    private element: HTMLElement;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-    }
-
+export default class DefinitionBlock extends BaseBlock {
     public getLevel(): CefrLevel | null {
         const levelStr = this.element.querySelector('.epp-xref')?.textContent;
         if (!levelStr) {
@@ -23,24 +15,5 @@ export default class DefinitionBlock implements Examplable, Definable {
      */
     public getCode(): string | null {
         return this.element.querySelector('.gram.dgram')?.textContent ?? null;
-    }
-
-    public getEnglishDefinition(): string | null {
-        return this.element.querySelector('.def')?.textContent ?? null;
-    }
-
-    public getZhTwDefinition(): string | null {
-        return this.element.querySelector('.def-body .trans')?.textContent ?? null;
-    }
-
-    public getExamples(): Example[] {
-        const exampleBlocks = this.element.querySelectorAll('.def-body .examp.dexamp');
-        const examples: Example[] = [];
-        exampleBlocks.forEach((exampleBlock) => {
-            const englishExample = filterBoldTextFromElement(exampleBlock.querySelector('.eg') as HTMLElement);
-            const zhTwExample = exampleBlock.querySelector('.trans')?.textContent ?? '';
-            examples.push({ englishExample, zhTwExample });
-        });
-        return examples;
     }
 }
