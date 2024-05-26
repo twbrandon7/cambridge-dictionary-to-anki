f6dcedb9580b8f6e7d77afac0eaab54049188dfa
Li-Xian Chen <twbrandon7@gmail.com>
2024-05-26 19:05:00 +0800

    feat(event): add event bus

---
+++ src/lib/event.ts
@@ -0,0 +1,47 @@
+import Example from "./example";
+
+export enum EventType {
+    OPEN_ANKI_CARD_MODAL = 'OPEN_ANKI_CARD_MODAL',
+};
+
+export interface Event {
+    eventType: EventType;
+}
+
+export class OpenAnkiCardModalEvent implements Event {
+    eventType: EventType = EventType.OPEN_ANKI_CARD_MODAL;
+    constructor(public readonly example: Example) {}
+}
+
+export type EventListener = (event: Event) => void;
+
+export default class EventBus {
+    private static instance: EventBus;
+    private events: Map<EventType, EventListener[]>;
+
+    private constructor() {
+        this.events = new Map();
+    }
+
+    static getInstance(): EventBus {
+        if (!EventBus.instance) {
+            EventBus.instance = new EventBus();
+        }
+        return EventBus.instance;
+    }
+
+    subscribe(eventType: EventType, callback: EventListener): void {
+        if (!this.events.has(eventType)) {
+            this.events.set(eventType, []);
+        }
+        this.events.get(eventType)?.push(callback);
+    }
+
+    publish(event: Event): void {
+        const eventType = event.eventType;
+        if (!this.events.has(eventType)) {
+            return;
+        }
+        this.events.get(eventType)?.forEach((callback) => callback(event));
+    }
+}
---
+++ test/lib/event.test.ts
@@ -0,0 +1,13 @@
+import { expect, test, vi } from "vitest";
+import EventBus, { EventType, OpenAnkiCardModalEvent } from "@/lib/event";
+import Example from "@/lib/example";
+
+test('test subscribe and publish', () => {
+    const eventBus = EventBus.getInstance();
+    const listener = vi.fn();
+    eventBus.subscribe(EventType.OPEN_ANKI_CARD_MODAL, listener);
+    const example = {} as Example;
+    eventBus.publish(new OpenAnkiCardModalEvent(example));
+    expect(listener).toHaveBeenCalledTimes(1);
+    expect(listener).toHaveBeenCalledWith(new OpenAnkiCardModalEvent(example));
+});
