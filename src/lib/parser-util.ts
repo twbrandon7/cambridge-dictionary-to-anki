0bbf4119d0b2631ad7f962ed3e8a3bed425d979c
CLX <twbrandon7@gmail.com>
2024-05-27 18:06:16 +0800

    fix(parser): fix the detection of node type

--- src/lib/parser-util.ts
+++ src/lib/parser-util.ts
@@ -2,7 +2,7 @@ export function filterBoldTextFromElement(element: HTMLElement): string {
     let result = "";
     element.childNodes.forEach((child: ChildNode) => {
         // if current has class `b`, then wrap it with <b> tag
-        if (child instanceof HTMLElement && child.classList.contains('b')) {
+        if (child.nodeType === Node.ELEMENT_NODE && (child as Element).classList.contains('b')) {
             result += `<b>${child.textContent}</b>`;
         } else {
             result += child.textContent;
