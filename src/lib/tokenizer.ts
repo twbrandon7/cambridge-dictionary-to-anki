1b86f32fb38a0313ad21c670d69008dce3a7b275
CLX <twbrandon7@gmail.com>
2024-05-27 22:36:45 +0800

    refactor(tokenizer): wrap parsed token by class

--- src/lib/tokenizer.ts
+++ src/lib/tokenizer.ts
@@ -1,13 +1,46 @@
+export class Token {
+  protected selected: boolean = false;
+
+  constructor(
+    protected readonly htmlStart: string | undefined,
+    protected readonly htmlEnd: string | undefined,
+    protected readonly word: string | undefined,
+    protected readonly notWord: string | undefined,
+  ) {}
+
+  public isWord(): boolean {
+      return this.word !== undefined && this.notWord === undefined;
+  }
+
+  public isHtmlTag(): boolean {
+      return this.htmlStart !== undefined || this.htmlEnd !== undefined;
+  }
+
+  public isSelected(): boolean {
+      return this.selected;
+  }
+
+  public toggleSelect(): void {
+      this.selected = !this.selected;
+  }
+
+  public getValue(): string {
+      return this.htmlStart ?? this.htmlEnd ?? this.word ?? this.notWord ?? '';
+  }
+}
+
 export default class Tokenizer {
-  static tokenize(text: string): string[] {
-    const regex = /(\w+)([^\w]+)?/gm;
+  static tokenize(text: string): Token[] {
+    const regex = /(?<html_start><\w+>)|(?<html_end><\/\w+>)|(?<word>\w+)|(?<not_word>[^\w])/gm;
     const tokens = [];
     let match;
     while ((match = regex.exec(text)) !== null) {
-      tokens.push(match[1]);
-      if (match[2]) {
-        tokens.push(match[2]);
+      const groups = match.groups;
+      if (groups === undefined) {
+        continue;
       }
+      const token = new Token(groups.html_start, groups.html_end, groups.word, groups.not_word);
+      tokens.push(token);
     }
     return tokens;
   }
--- test/lib/tokenizer.test.ts
+++ test/lib/tokenizer.test.ts
@@ -3,35 +3,80 @@ import Tokenizer from '@/lib/tokenizer';
 
 test('test tokenizer with punctuation', () => {
     const input = "This, is! an*example sentence. with punctuation.";
-    const expected = ["This", ", ", "is", "! ", "an", "*", "example", " ", "sentence", ". ", "with", " ", "punctuation", "."];
-    const result = Tokenizer.tokenize(input);
+    const expected = ["This", ",", " ", "is", "!", " ", "an", "*", "example", " ", "sentence", ".", " ", "with", " ", "punctuation", "."];
+    const result = Tokenizer.tokenize(input).map(token => token.getValue());
     expect(result).toEqual(expected);
 });
 
 test('test tokenizer with no punctuation', () => {
     const input = "This is an example sentence with no punctuation.";
     const expected = ["This", " ", "is", " ", "an", " ", "example", " ", "sentence", " ", "with", " ", "no", " ", "punctuation", "."];
-    const result = Tokenizer.tokenize(input);
+    const result = Tokenizer.tokenize(input).map(token => token.getValue());
     expect(result).toEqual(expected);
 });
 
 test('test tokenizer with sentence in the dictionary', () => {
     const input = "What time do you start/finish work?";
     const expected = ["What", " ", "time", " ",  "do", " ", "you", " ", "start", "/", "finish", " ", "work", "?"];
-    const result = Tokenizer.tokenize(input);
+    const result = Tokenizer.tokenize(input).map(token => token.getValue());
     expect(result).toEqual(expected);
 });
 
 test('test not ending by punctuation', () => {
     const input = "the economic/political situation";
     const expected = ["the", " ", "economic", "/", "political", " ", "situation"];
-    const result = Tokenizer.tokenize(input);
+    const result = Tokenizer.tokenize(input).map(token => token.getValue());
     expect(result).toEqual(expected);
 });
 
 test('test empty string', () => {
     const input = "";
     const expected: string[] = [];
-    const result = Tokenizer.tokenize(input);
+    const result = Tokenizer.tokenize(input).map(token => token.getValue());
     expect(result).toEqual(expected);
+});
+
+test('test tokenizer with html tags', () => {
+    const input = "She works <b>in</b> construction/in <b>the</b> construction <b>industry</b>.";
+    const expected = ["She", " ", "works", " ", "<b>", "in", "</b>", " ", "construction", "/", "in", " ", "<b>", "the", "</b>", " ", "construction", " ", "<b>", "industry", "</b>", "."];
+    const result = Tokenizer.tokenize(input).map(token => token.getValue());
+    expect(result).toEqual(expected);
+});
+
+test('test tokenizer end with html tag', () => {
+    const input = "She works <b>in</b> construction/in <b>the</b> construction <b>industry</b>";
+    const expected = ["She", " ", "works", " ", "<b>", "in", "</b>", " ", "construction", "/", "in", " ", "<b>", "the", "</b>", " ", "construction", " ", "<b>", "industry", "</b>"];
+    const result = Tokenizer.tokenize(input).map(token => token.getValue());
+    expect(result).toEqual(expected);
+});
+
+test('test tokenizer toggle', () => {
+    const input = "She works in construction.";
+    const result = Tokenizer.tokenize(input);
+    const firstToken = result[0];
+    expect(firstToken.isSelected()).toBe(false);
+    firstToken.toggleSelect();
+    expect(firstToken.isSelected()).toBe(true);
+});
+
+test('test token isWord', () => {
+    const input = "First <b>Fourth</b>.";
+    const tokens = Tokenizer.tokenize(input);
+    expect(tokens[0].isWord()).toBe(true);
+    expect(tokens[1].isWord()).toBe(false);
+    expect(tokens[2].isWord()).toBe(false);
+    expect(tokens[3].isWord()).toBe(true);
+    expect(tokens[4].isWord()).toBe(false);
+    expect(tokens[5].isWord()).toBe(false);
+});
+
+test('test token isHtml', () => {
+    const input = "First <b>Fourth</b>.";
+    const tokens = Tokenizer.tokenize(input);
+    expect(tokens[0].isHtmlTag()).toBe(false);
+    expect(tokens[1].isHtmlTag()).toBe(false);
+    expect(tokens[2].isHtmlTag()).toBe(true);
+    expect(tokens[3].isHtmlTag()).toBe(false);
+    expect(tokens[4].isHtmlTag()).toBe(true);
+    expect(tokens[5].isHtmlTag()).toBe(false);
 });
