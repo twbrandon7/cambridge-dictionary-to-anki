a6947eee222198f6bffe1cd5725bfb024c3e0c13
CLX <twbrandon7@gmail.com>
2024-07-14 14:33:47 +0800

    feat: add a service class to manage token exchange with server

---
+++ src/lib/services/token-exchange-service.ts
@@ -0,0 +1,55 @@
+import { AuthToken } from "../jwt-token";
+
+export default class TokenExchangeService {
+  constructor(private readonly endpoint: string) {}
+
+  public async login(key: string) {
+    const apiUrl = new URL("/api/v1/login", this.endpoint);
+    const response = await fetch(apiUrl.toString(), {
+      method: "POST",
+      headers: new Headers({ "content-type": "application/json" }),
+      body: JSON.stringify({
+        key: key,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("An error occurred while sending the request.");
+    }
+    const data = await response.json();
+    if (!data.accessToken || !data.refreshToken) {
+      throw new Error("Invalid response from server.");
+    }
+    return new AuthToken(data.accessToken, data.refreshToken);
+  }
+
+  public async refreshToken(refreshToken: string) {
+    const apiUrl = new URL("/api/v1/refresh", this.endpoint);
+    const response = await fetch(apiUrl.toString(), {
+      method: "POST",
+      headers: new Headers({ "content-type": "application/json" }),
+      body: JSON.stringify({
+        refreshToken: refreshToken,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("An error occurred while sending the request.");
+    }
+    const data = await response.json();
+    if (!data.accessToken || !data.refreshToken) {
+      throw new Error("Invalid response from server.");
+    }
+    return new AuthToken(data.accessToken, data.refreshToken);
+  }
+
+  public async healthCheck(accessToken: string) {
+    const apiUrl = new URL("/api/v1/health", this.endpoint);
+    const response = await fetch(apiUrl.toString(), {
+        method: "GET",
+        headers: new Headers({ "x-access-token": accessToken }),
+    });
+    if (!response.ok) {
+      throw new Error("An error occurred while sending the request.");
+    }
+    return response.json();
+  }
+}
